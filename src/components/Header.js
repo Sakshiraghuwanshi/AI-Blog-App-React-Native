6fad0a29d80b5225995ecacd1cb960765b064f5e
sakshiraghuwanshi <sakshi10072611@gmail.com>
2023-01-22 23:13:39 +0530

    Header

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View, Image,ScrollView } from "react-native";
+import React from "react";
+
+
+const Home = (props) => {
+  return (
+
+<View style={styles.mainview}>
+ <Text>Header</Text>
+  
+</View>
+
+  );
+  
+};
+
+const styles=StyleSheet.create({
+  
+})
+
+export default Home;
